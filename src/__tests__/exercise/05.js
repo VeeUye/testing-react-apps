8060580dde1433063afbf2568d9da9cd008aa940
Vee Uye <vee@accommodationforstudents.com>
2023-11-23 11:34:38 +0000

    Mocking HTTP requests: Reuse server request handlers

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -5,8 +5,10 @@ import * as React from 'react'
 import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import {build, fake} from '@jackfranklin/test-data-bot'
-import {rest, set} from 'msw'
+import {set} from 'msw'
 import {setupServer} from 'msw/node'
+
+import {handlers} from '../../test/server-handlers'
 
 import Login from '../../components/login-submission'
 
@@ -17,21 +19,7 @@ const buildLoginForm = build({
   },
 })
 
-const server = setupServer(
-  rest.post(
-    'https://auth-provider.example.com/api/login',
-    async (req, res, ctx) => {
-      if (!req.body.password) {
-        return res(ctx.status(400), ctx.json({message: 'password required'}))
-      }
-      if (!req.body.username) {
-        return res(ctx.status(400), ctx.json({message: 'username required'}))
-      }
-
-      return res(ctx.json({username: req.body.username}))
-    },
-  ),
-)
+const server = setupServer(...handlers)
 
 beforeAll(() => server.listen())
 afterAll(() => server.close())
