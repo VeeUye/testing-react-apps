dfb6bfbdae0ab8bc1d34a525bc2a9a0a6c054977
Vee Uye <vee@accommodationforstudents.com>
2023-11-23 12:14:34 +0000

    Mocking HTTP requests: Use one-off server handlers

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -5,13 +5,12 @@ import * as React from 'react'
 import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import {build, fake} from '@jackfranklin/test-data-bot'
-import {set} from 'msw'
+import {delay, rest, set} from 'msw'
 import {setupServer} from 'msw/node'
 
 import {handlers} from '../../test/server-handlers'
 
 import Login from '../../components/login-submission'
-import {createRenderer} from 'react-dom/test-utils'
 
 const buildLoginForm = build({
   fields: {
@@ -24,6 +23,7 @@ const server = setupServer(...handlers)
 
 beforeAll(() => server.listen())
 afterAll(() => server.close())
+afterEach(() => server.resetHandlers())
 
 it(`displays the username when the correct username and password have been submitted`, async () => {
   render(<Login />)
@@ -50,4 +50,32 @@ it(`triggers a login request failure when invalid login credentials have been su
   expect(screen.getByRole('alert').textContent).toMatchInlineSnapshot(
     `"password required"`,
   )
+})
+
+it('displays an error message when there is an unknown server error', async () => {
+  const testErrorMessage = 'oh no, something bad happened!'
+
+  server.use(
+    rest.post(
+      'https://auth-provider.example.com/api/login',
+      async (req, res, ctx) => {
+        return res(
+          ctx.delay(delay),
+          ctx.status(500),
+          ctx.json({message: testErrorMessage}),
+        )
+      },
+    ),
+  )
+
+  render(<Login />)
+
+  const {username} = buildLoginForm()
+
+  await userEvent.type(screen.getByLabelText(/username/i), username)
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+  expect(screen.getByRole('alert')).toHaveTextContent(testErrorMessage)
 })
